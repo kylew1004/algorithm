fa5a878046ccc984e113085779518d3e6ed6418b
GangYub <kylew1004@gmail.com>
2022-02-23 22:46:17 +0900

    큐

---
+++ Que/10845.py
@@ -0,0 +1,33 @@
+import sys
+
+Que = []
+N = int(sys.stdin.readline())
+
+for i in range(N):
+    order = sys.stdin.readline().split()
+    if order[0] == "push":
+        Que.append(order[1])
+    elif order[0] == "pop":
+        if len(Que) > 0:
+            print(Que[0])
+            del Que[0]
+        else:
+            print("-1")
+    elif order[0] == "size": 
+        print(len(Que))
+    elif order[0] == "empty":
+        if len(Que) > 0:
+            print("0")
+        else:
+            print("1")
+    elif order[0] == "front":
+        if len(Que) > 0:
+            print(Que[0])
+        else:
+            print("-1")
+    elif order[0] == "back":
+        if len(Que) > 0:
+            print(Que[-1])
+        else:
+            print("-1")
+    
