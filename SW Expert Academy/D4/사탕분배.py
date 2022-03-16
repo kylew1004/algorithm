0e200cd8aa2aec2893530adefd7c3f4f9eb9e305
GangYub <kylew1004@gmail.com>
2022-03-16 23:43:46 +0900

    바이러스

--- SW Expert Academy/D4/사탕분배.py
+++ SW Expert Academy/D4/사탕분배.py
@@ -1,8 +1,5 @@
 # https://swexpertacademy.com/main/code/problem/problemDetail.do?problemLevel=3&contestProbId=AX8BB5d6T7gDFARO&categoryId=AX8BB5d6T7gDFARO&categoryType=CODE&problemTitle=&orderBy=FIRST_REG_DATETIME&selectCodeLang=PYTHON&select-1=3&pageSize=10&pageIndex=1
 
-import sys
-
-input = sys.stdin.readline
 
 def result(a, b, k):
     if a == b:
@@ -12,7 +9,6 @@ def result(a, b, k):
         tmp = a if a < b else b
         a = a - tmp if tmp < a else a * 2
         b = b - tmp if tmp < b else b * 2
-        print(a, b)
     return min(a , b)
     
 N = int(input())
