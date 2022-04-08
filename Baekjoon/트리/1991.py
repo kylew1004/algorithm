a7561b1796ff084e2c4cf33ff76b5b47c2cc6691
GangYub <kylew1004@gmail.com>
2022-04-08 20:48:44 +0900

    트리 순회

--- Baekjoon/트리/1991.py
+++ Baekjoon/트리/1991.py
@@ -1,35 +1,36 @@
 # https://www.acmicpc.net/problem/1991
 
-def preorder(tree, now):
-    global res
-    res[0] += now
-    for next in tree[now]:
-        if next != '.':
-            preorder(tree, next)
-            
-def inorder(tree, now):
-    global res
-    if tree[now][0] != '.':
-        inorder(tree, tree[now][0])
-    res[1] += now
-    if tree[now][1] != '.':
-        inorder(tree, tree[now][1])
+import sys
+ 
+N = int(sys.stdin.readline().strip())
+tree = {}
+ 
+for n in range(N):
+    root, left, right = sys.stdin.readline().strip().split()
+    tree[root] = [left, right]
+ 
+ 
+def preorder(root):
+    if root != '.':
+        print(root, end='')  # root
+        preorder(tree[root][0])  # left
+        preorder(tree[root][1])  # right
+ 
+ 
+def inorder(root):
+    if root != '.':
+        inorder(tree[root][0])  # left
+        print(root, end='')  # root
+        inorder(tree[root][1])  # right
 
-def postorder(tree, now):
-    global res
-    for next in tree[now]:
-        if next != '.':
-            postorder(tree, next)
-    res[2] += now
-
-
-n = int(input())
-graph = dict()
-for i in range(n):
-    parent, left, right = input().split()
-    graph[parent] = [left, right]
-
-res = ['','','']
-for traversal in [preorder, inorder, postorder]:
-    traversal(graph, 'A')
-print('\n'.join(res))
+def postorder(root):
+    if root != '.':
+        postorder(tree[root][0])  # left
+        postorder(tree[root][1])  # right
+        print(root, end='')  # root
+        
+preorder('A')
+print()
+inorder('A')
+print()
+postorder('A')
