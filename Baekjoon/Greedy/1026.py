a58813257f2f6a7b70f360e37a6af14b36fa3d3b
GangYub <kylew1004@gmail.com>
2022-04-15 22:17:47 +0900

    보물

---
+++ Baekjoon/Greedy/1026.py
@@ -0,0 +1,15 @@
+# https://www.acmicpc.net/problem/1026
+
+import sys
+input = sys.stdin.readline
+
+n = int(input())
+a = list(map(int, input().split()))
+b = list(map(int, input().split()))
+result = 0
+
+for i in range(n):
+    result += min(a) * max(b)
+    a.pop(a.index(min(a)))
+    b.pop(b.index(max(b)))
+print(result)
