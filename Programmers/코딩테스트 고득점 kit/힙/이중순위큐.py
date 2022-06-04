8da7ffa398d769a19c322983810db2550db63e01
kylew1004 <kylew1004@gmail.com>
2022-06-04 23:01:45 +0900

    이중우선순위 큐

---
+++ Programmers/코딩테스트 고득점 kit/힙/이중순위큐.py
@@ -0,0 +1,28 @@
+# https://programmers.co.kr/learn/courses/30/lessons/42628
+
+import heapq
+
+def solution(operations):
+    answer = []
+    heap = []
+
+    for data in operations:
+        if data[0] == "I":
+            heapq.heappush(heap, int(data[2:]))
+        else:
+            if len(heap) == 0:
+                pass
+            elif data[2] == "-":
+                heapq.heappop(heap)
+            else:
+                heap = heapq.nlargest(len(heap), heap)[1:]
+                heapq.heapify(heap)
+
+    if heap:
+        answer.append(max(heap))
+        answer.append(min(heap))
+    else:
+        answer.append(0)
+        answer.append(0)
+
+    return answer
