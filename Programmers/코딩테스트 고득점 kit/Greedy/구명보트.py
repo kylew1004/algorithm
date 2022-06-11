3a82058c581b77adcec9ad3aa65de788163d35b0
kylew1004 <kylew1004@gmail.com>
2022-06-11 22:46:58 +0900

    구명보트

---
+++ Programmers/코딩테스트 고득점 kit/Greedy/구명보트.py
@@ -0,0 +1,16 @@
+# https://programmers.co.kr/learn/courses/30/lessons/42885
+
+def solution(people, limit):
+    answer = 0
+    
+    people.sort()
+    
+    start, end = 0, len(people) - 1
+    
+    while start <= end:
+        answer += 1
+        if people[start] + people[end] <= limit:
+            start += 1
+        end -= 1
+    
+    return answer
