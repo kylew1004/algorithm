2aaee5bb9f98a33c4b15bb0844bfaa2b8ac98f44
kylew1004 <kylew1004@gmail.com>
2022-07-10 04:49:00 +0900

    1244. [S/W 문제해결 응용] 2일차 - 최대 상금

---
+++ SW Expert Academy/d3/1244.py
@@ -0,0 +1,28 @@
+# https://swexpertacademy.com/main/code/problem/problemDetail.do?problemLevel=3&problemLevel=4&contcProbId=AV15Khn6AN0CFAYD&categoryId=AV15Khn6AN0CFAYD&categoryType=CODE&problemTitle=&orderBy=FIRST_REG_DATETIME&selectCodeLang=PYTHON&select-1=4&pageSize=10&pageIndex=1
+
+def dfs(idx, cnt):
+    global answer
+
+    if cnt == int(target):
+        answer = max(int("".join(num)), answer)
+        return
+    for now in range(idx, n):
+        for max_idx in range(now + 1, n):
+            if num[now] <= num[max_idx]:
+                num[now], num[max_idx] = num[max_idx], num[now]
+                dfs(now, cnt + 1)
+                num[now], num[max_idx] = num[max_idx], num[now]
+    if not answer and cnt < int(target):
+        rotate = (int(target) - cnt) % 2
+        if rotate:
+            num[-1], num[-2] = num[-2], num[-1]
+        dfs(idx, int(target))
+
+
+for tc in range(1, int(input()) + 1):
+    num, target = input().split()
+    n = len(num)
+    num = list(num)
+    answer = 0
+    dfs(0, 0)
+    print(f'#{tc} {answer}')
