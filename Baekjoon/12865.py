2d7e4b02f78f1b705c7ce82c363819451d0c6f39
kylew1004 <kylew1004@gmail.com>
2022-08-03 22:30:45 +0900

    평범한 배낭

---
+++ Baekjoon/12865.py
@@ -0,0 +1,10 @@
+# https://www.acmicpc.net/problem/12865
+
+n, k = map(int, input().split())
+cache = [0] * (k+1)
+
+for _ in range(n):
+    w, v = map(int, input().split())
+    for j in range(k, w-1, -1):
+        cache[j] = max(cache[j], cache[j-w] + v)
+print(cache[-1])
