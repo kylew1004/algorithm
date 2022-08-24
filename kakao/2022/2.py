303cf28263cd5e727f54dbafc2f70428c23b1721
kylew1004 <kylew1004@gmail.com>
2022-08-24 20:29:14 +0900

    https://school.programmers.co.kr/learn/courses/30/lessons/118667

---
+++ kakao/2022/2.py
@@ -0,0 +1,25 @@
+# https://school.programmers.co.kr/learn/courses/30/lessons/118667
+
+from collections import deque
+
+def solution(queue1, queue2):
+    queue1 = deque(queue1)
+    queue2 = deque(queue2)
+    answer = 0
+    num = queue1 + queue2
+    cnt = sum(num) / 2
+    queue1_sum = sum(queue1)
+    
+    for _ in range(len(num) * 2):
+        if queue1_sum < cnt:
+            queue1_sum += queue2[0]
+            queue1.append(queue2.popleft())
+            answer += 1
+        elif queue1_sum > cnt:
+            queue1_sum -= queue1[0]
+            queue2.append(queue1.popleft())
+            answer += 1
+        else:
+            return answer
+
+    return -1
