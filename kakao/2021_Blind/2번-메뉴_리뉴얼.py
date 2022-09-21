d22c4534b1d12b6ec88f50a87be83c68ad0b76a6
kylew1004 <kylew1004@gmail.com>
2022-09-21 20:19:19 +0900

    메뉴 리뉴얼

---
+++ kakao/2021_Blind/2번-메뉴_리뉴얼.py
@@ -0,0 +1,35 @@
+# https://school.programmers.co.kr/learn/courses/30/lessons/72411
+
+from itertools import combinations
+
+def solution(orders, course):
+    answer = []
+    order_dict = {}
+
+    for i in range(len(course)):
+        for j in range(len(orders)):
+            for c in combinations(orders[j], course[i]):
+                cc = list(c)
+                cc.sort()
+                tmp = ''.join(cc)
+                if tmp in order_dict:
+                    order_dict[tmp] += 1
+                else:
+                    order_dict[tmp] = 1
+
+    for c in course:
+        arr = []
+        maxNum = 2
+        for k, v in order_dict.items():
+            if len(k) == c:
+                if maxNum < v:
+                    arr = [k]
+                    maxNum = v
+                elif maxNum == v:
+                    arr.append(k)
+        for i in range(len(arr)):
+            answer.append(arr[i])
+
+    answer.sort()
+
+    return answer
