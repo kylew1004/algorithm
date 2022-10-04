0978a36bbd3b05bf36302f64b29354a3e960eabd
kylew1004 <kylew1004@gmail.com>
2022-10-04 22:04:28 +0900

    행렬

---
+++ Baekjoon/Greedy/1080.py
@@ -0,0 +1,36 @@
+# https://www.acmicpc.net/problem/1080
+
+n, m = map(int, input().split())
+graph1 = []
+graph2 = []
+count = 0
+
+def reverse(i, j):
+    for x in range(i, i + 3):
+        for y in range(j, j + 3):
+            graph1[x][y] = 1 - graph1[x][y]
+
+
+for i in range(n):
+    graph1.append(list(map(int, input())))
+
+for i in range(n):
+    graph2.append(list(map(int, input())))
+
+for i in range(n - 2):
+    for j in range(m - 2):
+        if graph1[i][j] != graph2[i][j]:
+            reverse(i, j)
+            count += 1
+flag = 0
+
+for i in range(n):
+    for j in range(m):
+        if graph1[i][j] != graph2[i][j]:
+            flag = 1
+            break
+
+if flag == 1:
+    print(-1)
+else:
+    print(count)
