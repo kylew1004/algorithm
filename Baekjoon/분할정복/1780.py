05f53c24d5f24223c784bc843e4f5982d783beab
kylew1004 <kylew1004@gmail.com>
2022-10-14 08:29:38 +0900

    종이의 개수(수정)

--- Baekjoon/분할정복/1780.py
+++ Baekjoon/분할정복/1780.py
@@ -1,27 +1,32 @@
 # https://www.acmicpc.net/problem/1780
 
+import sys
+sys.setrecursionlimit(10 ** 5)
+
 n = int(input())
 graph = [list(map(int, input().split())) for _ in range(n)]
 count = [0, 0, 0]
 
 
 def divide(x, y, num):
-    num = graph[x][y]
-    for i in range(x, x + n):
-        for j in range(y, y + n):
-            if graph[i][j] != num:
+    global count
+    visited = graph[x][y]
+
+    for i in range(x, x + num):
+        for j in range(y, y + num):
+            if graph[i][j] != visited:
                 for a in range(3):
                     for b in range(3):
-                        divide(x + a * (n // 3), y + b * (n // 3), n // 3)
+                        divide(x + a * num // 3, y + b * num // 3, num // 3)
                 return
 
-    if num == -1:
+    if visited == -1:
         count[0] += 1
-    elif num == 0:
+    elif visited == 0:
         count[1] += 1
     else:
         count[2] += 1
 
 
 divide(0, 0, n)
-print(count[i] for i in range(3))
+[print(i) for i in count]
