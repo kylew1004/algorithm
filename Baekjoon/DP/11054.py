644e1fffca6b836254d8ff6879fac41dfc87944e
kylew1004 <kylew1004@gmail.com>
2022-10-27 22:06:44 +0900

    가장 긴 바이토닉 부분 수열

--- Baekjoon/DP/11054.py
+++ Baekjoon/DP/11054.py
@@ -1,22 +1,24 @@
 # https://www.acmicpc.net/problem/11054
+# 1107코드를 잘못 커밋함;;
 
 import sys
 input = sys.stdin.readline
 
 n = int(input())
-m = int(input())
-buttons = list(input().split())
-answer = abs(n - 100)
+arr = list(map(int, input().split()))
+up = [1 for _ in range(n)]
+down = [1 for _ in range(n)]
+result = [0 for _ in range(n)]
 
-def check(num):
-    nums = list(str(num))
-    for i in nums:
-        if i in buttons:
-            return 0
-    return 1
+for i in range(n):
+    for j in range(i):
+        if arr[i] > arr[j] and up[i] < up[j] + 1:
+            up[i] = up[j] + 1
 
-for num in range(1000001):
-    if check(num):
-        answer = min(answer, len(str(num)) + abs(num - n))
+for i in range(n - 1, -1, -1):
+    for j in range(i + 1, n):
+        if arr[i] > arr[j] and down[i] < down[j] + 1:
+            down[i] = down[j] + 1
+    result[i] = down[i] + up[i] - 1
 
-print(answer)
+print(max(result))
