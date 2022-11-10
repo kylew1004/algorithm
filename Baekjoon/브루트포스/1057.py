5501e6bce83bc7dbaa5a5937863e68e030a8dc3e
kylew1004 <kylew1004@gmail.com>
2022-11-10 22:56:34 +0900

    토너먼트

---
+++ Baekjoon/브루트포스/1057.py
@@ -0,0 +1,11 @@
+# https://www.acmicpc.net/problem/1057
+
+n, start, end = map(int, input().split())
+cnt = 0
+
+while start != end:
+    start -= start//2
+    end -= end//2
+    cnt +=1
+
+print(cnt)
