b53d7a3ca9959bdb02a357e8cb0324f179d36b13
kylew1004 <kylew1004@gmail.com>
2022-11-21 22:21:02 +0900

    최대 힙

---
+++ Baekjoon/Que/11279.py
@@ -0,0 +1,18 @@
+# https://www.acmicpc.net/problem/11279
+
+import sys
+import heapq
+input = sys.stdin.readline
+
+n = int(input())
+heap = []
+
+for i in range(n):
+    a = int(input())
+    if a == 0:
+        if heap:
+            print(-1 * heapq.heappop(heap))
+        else:
+            print(0)
+    else:
+        heapq.heappush(heap, -1 * a)
