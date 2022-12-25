93c8cba0949ece8d9b7fb27cbcbe637f8e2ca7e7
kylew1004 <kylew1004@gmail.com>
2022-12-25 22:32:18 +0900

    A → B

---
+++ Baekjoon/Greedy/16953.py
@@ -0,0 +1,26 @@
+# https://www.acmicpc.net/problem/16953
+
+import sys
+
+input = sys.stdin.readline
+
+a, b = map(int, input().split())
+count = 1
+
+while True:
+    if a == b:
+        break
+    elif a > b:
+        count = -1
+        break
+    elif b % 10 == 1:
+        b //= 10
+        count += 1
+    elif b % 2 == 0:
+        b //= 2
+        count += 1
+    else:
+        count = -1
+        break
+
+print(count)
