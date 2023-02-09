b09ea38c9cfb42d2c7f15352abf914c844fdc442
kylew1004 <kylew1004@gmail.com>
2023-02-09 23:02:10 +0900

    카잉 달력

---
+++ Baekjoon/브루트포스/6064.py
@@ -0,0 +1,17 @@
+# https://www.acmicpc.net/problem/6064
+
+import sys
+input = sys.stdin.readline
+
+t = int(input())
+
+for _ in range(t):
+    m, n, x, y = map(int, input().split())
+    ans = -1
+
+    while x <= m * n:
+        if (x - y) % n == 0:
+            ans = x
+            break
+        x += m
+    print(ans)   
