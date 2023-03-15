c400c06e40e85c2a7a89eeca914ba0cb70bbb031
kylew1004 <kylew1004@gmail.com>
2023-03-15 20:53:34 +0900

    124 나라의 숫자

---
+++ Programmers/Level2/124_나라의_숫자.py
@@ -0,0 +1,11 @@
+# https://school.programmers.co.kr/learn/courses/30/lessons/12899
+
+def solution(n):
+    answer = ''
+    numbers = ['4', '1', '2']
+
+    while n:
+        answer = numbers[n % 3] + answer
+        n = n // 3 - (n % 3 == 0)
+
+    return answer
