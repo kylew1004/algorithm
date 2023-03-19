bd20679d674ecd7e3aa59ee8412890e05800e4a8
kylew1004 <kylew1004@gmail.com>
2023-03-19 23:36:53 +0900

    N번째 큰 수

---
+++ Baekjoon/Que/2075.py
@@ -0,0 +1,15 @@
+# https://www.acmicpc.net/problem/2075
+
+import heapq
+
+hq = []
+n = int(input())
+
+for _ in range(n):
+    for i in map(int, input().split()):
+        if len(hq) >= n:
+            heapq.heappushpop(hq, i)
+        else:
+            heapq.heappush(hq, i)
+
+print(heapq.heappop(hq))
