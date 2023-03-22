81214dbd15cc30f834bc2bec3201a4749c06caad
kylew1004 <kylew1004@gmail.com>
2023-03-22 22:57:43 +0900

    하샤드 수

---
+++ Programmers/Level1/하샤드_수.py
@@ -0,0 +1,12 @@
+# https://school.programmers.co.kr/learn/courses/30/lessons/12947
+
+def solution(x):
+    sum = 0
+
+    for i in str(x):
+        sum += int(i)
+
+    if x % sum == 0:
+        return True
+    else:
+        return False
