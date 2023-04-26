464c327d8e2a34ce2abe01c61c635779de150084
kylew1004 <kylew1004@gmail.com>
2023-04-26 22:31:21 +0900

    입국심사

---
+++ Programmers/코딩테스트 고득점 kit/이분탐색/입국심사.py
@@ -0,0 +1,22 @@
+# https://school.programmers.co.kr/learn/courses/30/lessons/43238
+
+def solution(n, times):
+    answer = 0
+    left, right = 1, max(times) * n
+
+    while left <= right:
+        mid = (left + right) // 2
+        cnt = 0
+
+        for time in times:
+            cnt += mid // time
+            if answer == n:
+                break
+
+        if cnt >= n:
+            answer = mid
+            right = mid - 1
+        elif cnt < n:
+            left = mid + 1
+
+    return answer
