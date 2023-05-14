35db12044b671f5be17c850d381bb8ad17271999
kylew1004 <kylew1004@gmail.com>
2023-05-14 18:00:02 +0900

    boj_1094 막대기 (python)

---
+++ Baekjoon/비트마스킹/1094.py
@@ -0,0 +1,14 @@
+# https://www.acmicpc.net/problem/1094
+
+import sys
+input = sys.stdin.readline
+
+x = int(input())
+cnt = 0
+
+while x > 0:
+    if x % 2 == 1:
+        cnt += 1
+    x //= 2
+
+print(cnt)
