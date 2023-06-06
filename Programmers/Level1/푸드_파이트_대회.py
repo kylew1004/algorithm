73cc5b221abf1f80539b0f9aadf3e49d826b0e96
kylew1004 <kylew1004@gmail.com>
2023-06-06 17:50:49 +0900

    programmers_푸드 파이트 대회 (python)

---
+++ Programmers/Level1/푸드_파이트_대회.py
@@ -0,0 +1,8 @@
+# https://school.programmers.co.kr/learn/courses/30/lessons/134240
+
+def solution(food):
+    answer = ''
+    for i in range(1, len(food)):
+        answer += str(i) * (food[i] // 2)
+
+    return answer + '0' + answer[::-1]
