414d5595ecdb1a5659952a302e34709440264606
kylew1004 <kylew1004@gmail.com>
2023-07-27 22:51:31 +0900

    boj_1436 영화감독 숌 (python)

---
+++ Baekjoon/브루트포스/1436.py
@@ -0,0 +1,16 @@
+# https://www.acmicpc.net/problem/1436
+
+import sys
+input = sys.stdin.readline
+
+n = int(input())
+num = 666
+
+while True:
+    if '666' in str(num):
+        n -= 1
+        if n == 0:
+            break
+    num += 1
+
+print(num)
