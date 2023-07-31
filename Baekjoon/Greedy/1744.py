5980953add393789119542a3761f4db35fa25429
kylew1004 <kylew1004@gmail.com>
2023-07-31 22:39:44 +0900

    boj_1744 DNA (python)

---
+++ Baekjoon/Greedy/1744.py
@@ -0,0 +1,24 @@
+# https://www.acmicpc.net/problem/1744
+
+import sys
+input = sys.stdin.readline
+
+n, m = map(int, input().split())
+
+data = []
+for i in range(n):
+    data.append(input())
+dna = ''
+answer = 0
+
+data.sort()
+
+for y in range(m):
+    value = {'A' : 0, 'C' : 0, 'G' : 0, 'T' : 0}
+    for x in range(n):
+        value[data[x][y]] += 1
+    dna += max(value, key=value.get)
+    answer += n - max(value.values())
+
+print(dna)
+print(answer)
