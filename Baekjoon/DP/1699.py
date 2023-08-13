bec92d475246ea6dd4b392db06da8c57cc57f8ba
kylew1004 <kylew1004@gmail.com>
2023-08-13 23:24:51 +0900

    boj_1699 제곱수의 합 (python)

--- Baekjoon/DP/1699.py
+++ Baekjoon/DP/1699.py
@@ -1,17 +1,16 @@
 # https://www.acmicpc.net/problem/1699
 
+# https://www.acmicpc.net/problem/1699
+
 import sys
 input = sys.stdin.readline
- 
+
 n = int(input())
- 
-dp = [i for i in range (n+1)]
- 
-for i in range(1, n+1):
-    for j in range(1, i):
-        if (j * j) > i:
-            break
-        if dp[i] > dp[i - j * j] + 1:
-            dp[i] = dp[i - j * j] + 1
- 
+
+dp = [i for i in range(n + 1)]
+
+for i in range(2, int(n ** 0.5) + 1):
+    for j in range(i * i, n + 1):
+        dp[j] = min(dp[j], dp[j - i * i] + 1)
+
 print(dp[n])
