66d416ecd75349051b45dcdfbbfe86d435de1a0b
kylew1004 <kylew1004@gmail.com>
2023-09-21 23:29:49 +0900

    boj_2747 피보나치 수 (python)

---
+++ Baekjoon/DP/2747.py
@@ -0,0 +1,15 @@
+# https://www.acmicpc.net/problem/2747
+
+import sys
+input = sys.stdin.readline
+
+n = int(input().rstrip())
+dp = [0] * (n + 1)
+
+for i in range(1, n + 1):
+    if i == 1:
+        dp[i] = 1
+    else:
+        dp[i] = dp[i - 1] + dp[i - 2]
+        
+print(dp[n])
