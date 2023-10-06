57d6dd3b5b5d0cf8c78fa51767f989f97484955e
kylew1004 <kylew1004@gmail.com>
2023-10-06 19:03:47 +0900

    boj_1259 팰린드롬수 (python)

---
+++ Baekjoon/문자열/1259.py
@@ -0,0 +1,13 @@
+# https://www.acmicpc.net/problem/1259
+
+import sys
+input = sys.stdin.readline
+
+while True:
+    n = input().rstrip()
+    if n == '0':
+        break
+    if n == n[::-1]:
+        print('yes')
+    else:
+        print('no')
