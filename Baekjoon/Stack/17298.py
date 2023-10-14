31715dd6f061eb849c4423143315b30afef60e07
kylew1004 <kylew1004@gmail.com>
2023-10-14 20:27:05 +0900

    boj_17298 오큰수 (python)

---
+++ Baekjoon/Stack/17298.py
@@ -0,0 +1,16 @@
+# https://www.acmicpc.net/problem/17298
+
+import sys
+input = sys.stdin.readline
+
+n = int(input())
+arr = list(map(int, input().split()))
+stack = []
+result = [-1] * n
+
+for i in range(n):
+    while stack and arr[stack[-1]] < arr[i]:
+        result[stack.pop()] = arr[i]
+    stack.append(i)
+
+print(*result)
