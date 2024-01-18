d3d5f1842a113424e4d75fb05dd3a8689f3a2e4f
kylew1004 <kylew1004@gmail.com>
2024-01-18 23:17:07 +0900

    boj_17219 비밀번호 찾기 (python)

---
+++ Baekjoon/딕셔너리/17219.py
@@ -0,0 +1,14 @@
+# https://www.acmicpc.net/problem/17219
+
+import sys
+input = sys.stdin.readline
+
+n, m = map(int, input().split())
+dic = {}
+
+for _ in range(n):
+    site, pw = input().split()
+    dic[site] = pw
+
+for _ in range(m):
+    print(dic[input().rstrip()])
