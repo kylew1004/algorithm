5bc61cafdad16ec2795478a28210eab586b247d0
kylew1004 <kylew1004@gmail.com>
2024-01-30 18:00:52 +0900

    boj_이항 계수 1 (python)

---
+++ Baekjoon/수학/11050.py
@@ -0,0 +1,13 @@
+# https://www.acmicpc.net/problem/11050
+
+import sys
+input = sys.stdin.readline
+
+n, k = map(int, input().split())
+
+def factorial(n):
+    if n == 0:
+        return 1
+    return n * factorial(n - 1)
+
+print(factorial(n) // (factorial(k) * factorial(n - k)))
