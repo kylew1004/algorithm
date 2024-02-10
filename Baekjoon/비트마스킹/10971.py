781d384553d2a3a3068faf887dad51e71ba4414d
kylew1004 <kylew1004@gmail.com>
2024-02-10 20:37:22 +0900

    boj_10971 외판원 순회 2 (python)

---
+++ Baekjoon/비트마스킹/10971.py
@@ -0,0 +1,22 @@
+# https://www.acmicpc.net/problem/10971
+# 2098 외판원 순회랑 같은 문제지만 메모리 제한과 시간 제한이 2배이다.
+
+import sys
+input = sys.stdin.readline
+
+n = int(input())
+w = [list(map(int, input().split())) for _ in range(n)]
+dp = [[-1] * (1 << n) for _ in range(n)]
+
+def tsp(here, visited):
+    if visited == (1 << n) - 1:
+        return w[here][0] if w[here][0] else float('inf')
+    if dp[here][visited] != -1:
+        return dp[here][visited]
+    dp[here][visited] = float('inf')
+    for i in range(n):
+        if visited & (1 << i) == 0 and w[here][i] > 0:
+            dp[here][visited] = min(dp[here][visited], tsp(i, visited | (1 << i)) + w[here][i])
+    return dp[here][visited]
+
+print(tsp(0, 1))
