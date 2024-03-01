6c40dd966eb805d63df573096c44dae1f2ef887b
kylew1004 <kylew1004@gmail.com>
2024-03-01 23:37:16 +0900

    boj_11382 꼬마 정민 (python)

---
+++ Baekjoon/구현/11382.py
@@ -0,0 +1,8 @@
+# https://www.acmicpc.net/problem/11382
+# 갑자기 github 잔디가 안심어져서 테스트용으로 올림, programmers 미로 탈출 올렸음.
+
+import sys
+input = sys.stdin.readline
+
+a, b, c = map(int, input().split())
+print(a + b + c)
