9f83dd5e2b3fc3e0791a2bb1da6111d2b9545b00
kylew1004 <kylew1004@gmail.com>
2024-03-11 23:19:13 +0900

    boj_1766 문제집 (python)

---
+++ Baekjoon/Heap/1766.py
@@ -0,0 +1,28 @@
+#t https://www.acmicpc.net/problem/1766
+
+import sys
+import heapq
+input = sys.stdin.readline
+
+n, m = map(int, input().split())
+degree = [0] * (n + 1)
+graph = [[] for _ in range(n + 1)]
+
+for _ in range(m):
+    a, b = map(int, input().split())
+    graph[a].append(b)
+    degree[b] += 1
+    
+q = []
+for i in range(1, n + 1):
+    if degree[i] == 0:
+        heapq.heappush(q, i)
+        
+while q:
+    now = heapq.heappop(q)
+    print(now, end=' ')
+    
+    for i in graph[now]:
+        degree[i] -= 1
+        if degree[i] == 0:
+            heapq.heappush(q, i)
