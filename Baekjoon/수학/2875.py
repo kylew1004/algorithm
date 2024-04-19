b78885756f3fe89d93bf711b016688eed347c0a7
kylew1004 <kylew1004@gmail.com>
2024-04-19 23:32:56 +0900

    boj_2875 대회 or 인턴 (python)

---
+++ Baekjoon/수학/2875.py
@@ -0,0 +1,14 @@
+# https://www.acmicpc.net/problem/2875
+
+import sys
+input = sys.stdin.readline
+
+n, m, k = map(int, input().split())
+team = 0
+
+while n >= 2 and m >= 1 and n + m >= k + 3:
+    n -= 2
+    m -= 1
+    team += 1
+    
+print(team)
