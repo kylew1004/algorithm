9670501925ce0ab9e71ef105c041f3762affa8d8
kylew1004 <kylew1004@gmail.com>
2024-06-10 16:37:29 +0900

    boj_2753 윤년 (c++)

---
+++ Baekjoon/구현/2753.cpp
@@ -0,0 +1,15 @@
+// https://www.acmicpc.net/problem/2753
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    int year;
+    cin >> year;
+    if (year % 4 == 0 && (year % 100 != 0 || year % 400 == 0)) {
+        cout << 1;
+    } else {
+        cout << 0;
+    }
+    return 0;
+}
