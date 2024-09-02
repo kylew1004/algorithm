fb884290531d72f065f0726fdde4a9134e561508
kylew1004 <kylew1004@gmail.com>
2024-09-02 23:28:47 +0900

    boj_10926 ??! (c++)

---
+++ Baekjoon/구현/10926.cpp
@@ -0,0 +1,14 @@
+// https://www.acmicpc.net/problem/10926
+
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main() {
+    string id;
+    cin >> id;
+
+    cout << id << "??!" << endl;
+
+    return 0;
+}
