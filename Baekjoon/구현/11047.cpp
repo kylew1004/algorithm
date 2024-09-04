5b4b316bf44deac40a5d3b478c6d68117ca2b975
kylew1004 <kylew1004@gmail.com>
2024-09-04 23:35:08 +0900

    boj_11047 동전 0 (c++)

---
+++ Baekjoon/구현/11047.cpp
@@ -0,0 +1,25 @@
+// https://www.acmicpc.net/problem/11047
+
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int main() {
+    int n, k;
+    cin >> n >> k;
+
+    vector<int> coins(n);
+    for (int i = 0; i < n; i++) {
+        cin >> coins[i];
+    }
+
+    int count = 0;
+    for (int i = n - 1; i >= 0; i--) {
+        count += k / coins[i];
+        k %= coins[i];
+    }
+
+    cout << count << endl;
+
+    return 0;
+}
